d7119785f1724cef0e37ba99cd3a691ae533bae1
Thomas Ferreira <thomas.devfs@gmail.com>
2022-09-20 15:20:42 -0300

    Definindo props de forma mais breve

--- react-one/src/components/Pessoa.js
+++ react-one/src/components/Pessoa.js
@@ -1,12 +1,12 @@
-function Pessoa (props) {
+function Pessoa ({nome, idade, profissao, foto}) {
     return(
         <div>
             
-            <h2>Nome: {props.nome}</h2>
-            <p>Idade: {props.idade}</p>
-            <p>Profissão: {props.profissao}</p>
-            <img src={props.foto} alt={props.nome}/>
-            
+            <h2>Nome: {nome}</h2>
+            <p>Idade: {idade}</p>
+            <p>Profissão: {profissao}</p>
+            <img src={foto} alt={nome}/>
+
         </div>
     )
 }
